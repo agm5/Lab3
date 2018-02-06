169502d81cf4b98d4e372fc5e2b76a9ab21a7892
agm5 <agm5@gmail.com>
2018-02-06 14:41:27 -0600

    Did it

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+import java.lang.*;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        // write your code here
+        wordCount("http://erdani.com/tdpl/hamlet.txt");
+    }
+
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    public static void wordCount(String url) {
+        int wc = 1;
+        String urlString = urlToString(url);
+        for (int i = 0; i < urlString.length(); i++) {
+            if (urlString.charAt(i) == ' ') {
+                wc += 1;
+            }
+        }
+        System.out.print("Word Count: "+wc);
+    }
+}
